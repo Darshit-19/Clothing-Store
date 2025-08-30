2a4b1b3a1300e1b12ecb82ebfa058c07175b6482
Darshit Hairiya <hairiyadarshit@gmail.com>
2025-08-30 13:50:27 +0530

    route setup for get products

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,9 +1,22 @@
 import express from "express";
-import verifyToken from "../middlewares/auth.middleware.js";
-import { addProduct } from "../controllers/product.controller.js";
+import {
+  isAuthenticated,
+  authorizeRoles,
+} from "../middlewares/auth.middleware.js";
+import { addProduct, getProduct } from "../controllers/product.controller.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import { addProductSchema } from "../validation/product.validation.js";
 
 const router = express.Router();
 
-router.post("/products", verifyToken,addProduct);
+router.post(
+  "/products",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  validate(addProductSchema),
+  addProduct
+);
+
+router.get("/products", isAuthenticated, getProduct);
 
 export default router;
