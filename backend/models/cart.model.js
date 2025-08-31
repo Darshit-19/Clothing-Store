8ffb43474131455f0d49a01459b4f9e0f6f70ae7
Darshit Hairiya <hairiyadarshit@gmail.com>
2025-08-31 13:30:52 +0530

    cart model added

---
+++ backend/models/cart.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const cartItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    min: 1,
+    required: true,
+  },
+  selectedSize: {
+    type: String,
+    enum: ["XS", "S", "M", "L", "XL"],
+    required: true,
+  },
+});
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [cartItemSchema],
+    status: {
+      type: String,
+      enum: ["active", "ordered"],
+      default: "active",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Cart", cartSchema);
