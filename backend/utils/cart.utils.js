b5881cd09110e10482dac7c1e18fc45fc7407bd7
Darshit Hairiya <hairiyadarshit@gmail.com>
2025-09-02 10:13:36 +0530

    helper function for calculating cart total created

---
+++ backend/utils/cart.utils.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import Product from '../models/product.model'
+
+export const calculateCartTotal = async (items) => {
+    let total = 0
+
+    for(const item of items){
+        const product = await Product.findById(item.productId)
+        if(product){
+            total += item.quantity * product.price
+        }
+    }
+    return total
+}
