1517a77d4c56f65938e0dc90433b33ca82519ff1
Darshit Hairiya <hairiyadarshit@gmail.com>
2025-09-02 18:19:43 +0530

    controller function added for viewing order by the user

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -28,12 +28,32 @@ export const placeOrder = async (req, res) => {
 
     await order.save();
 
-    cart.status = "ordered"
+    cart.status = "ordered";
     await cart.save();
 
     res.status(201).json({ message: "Order succesfully placed", order });
   } catch (error) {
     console.error("Error in placing order: ", error.message);
     res.status(500).json({ message: "Somthing went wrong" });
+  }
+};
+
+export const viewMyOrder = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const orders = await Order.findOne({ userId })
+      .populate("items.productId", "name", "price", "image")
+      .sort({ createdAt: -1 });
+    if (!orders || orders.length === 0) {
+      return res.status(404).json({ message: "No order for the user" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Order fetched succesfully", orders });
+  } catch (error) {
+    console.error("Error while fetching orders", error.message);
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
